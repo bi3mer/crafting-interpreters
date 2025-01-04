34d5528d41824eb05d13db033232364f7aeddc4b
bi3mer <colan@comcast.net>
2025-01-04 14:35:16 -0500

    read input from file or user

--- tree-walk/src/main.cpp
+++ tree-walk/src/main.cpp
@@ -1,5 +1,50 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string.h>
 
-int main() {
-    printf("hello, world!\n");
+void error(int line, std::string message) {
+
+}
+
+void run(const std::string& source) {
+    std::cout << source << std::endl;
+}
+
+void run_prompt() {
+    std::string line;
+    for(;;) {
+        printf("> ");
+        getline(std::cin, line);
+        if (line.empty()) {
+            break;
+        }
+
+        run(line);
+    }
+}
+
+void run_file(const char *script_file) {
+    std::ifstream file(script_file);
+    if (!file.is_open()) {
+        printf("Script file not found: %s\n", script_file);
+        exit(1);
+    }
+
+    std::stringstream source_stream;
+    source_stream << file.rdbuf();
+    run(source_stream.str());
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 2) {
+        printf("Usage: lox [script]\n");
+        exit(1);
+    } else if (argc == 2) {
+        run_file(argv[1]);
+    } else {
+        run_prompt();
+    }
 }
